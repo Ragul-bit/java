f18be858000f355bf52d51679275d26c5a5de282
Ragul-bit <ragul2005badge@gmail.com>
2025-09-18 09:30:11 +0530

    Add files via upload

---
+++ switch1.java
@@ -0,0 +1,17 @@
+public class switch1 {
+    public static void main(String[] args) {
+        int choice =6;
+        switch(choice){
+            case 1: System.out.println("1.sunday");break;
+            case 2: System.out.println("2.monday");break;
+            case 3: System.out.println("3.tuesday");break;
+            case 4: System.out.println("4.wednesday");break;
+            case 5: System.out.println("5.thursday");break;
+            case 6: System.out.println("6.friday");break;
+            case 7: System.out.println("7.saturday");break;
+            default:System.out.println("invalid number entened,enter number from 1-7");
+            }
+        }
+    }
+    
+
